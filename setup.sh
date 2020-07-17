d9ba08dcdef313804827e0e795588e5b0074a9fb
qwerty2501 <qwerty2501@live.jp>
2020-07-18 01:54:46 +0900

    added calling ansible-playbook.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+set -eu
+
+wd=$(dirname $0)
+cd $wd
 
 sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt update
 sudo apt install -y ansible
+
+ansible-playbook setup.yaml
