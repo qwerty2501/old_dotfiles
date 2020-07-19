dcdc8b9e2acf70daca2c117cc01aee4e34e6a27c
qwerty2501 <qwerty2501@live.jp>
2020-07-19 23:25:26 +0900

    removed preparing for ansible.

--- setup.sh
+++ setup.sh
@@ -5,8 +5,6 @@ wd=$(dirname $0)
 cd $wd
 
 sudo apt update
-sudo apt install --yes software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install --yes ansible
 
 ansible-galaxy install robertdebock.cargo
