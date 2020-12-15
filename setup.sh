e31dab074bbe7f3f7c34473c812e489f62620b70
qwerty2501 <qwerty2501@live.jp>
2020-12-15 16:10:23 +0900

    big modified.

--- setup.sh
+++ setup.sh
@@ -7,12 +7,17 @@ cd $wd
 
 ask_become_pass="--ask-become-pass"
 skip_tags=''
+force_install="false"
 for OPT in "$@"
 do
 	case $OPT in
+				--force-install)
+			force_install=true
+			shift 1;;
+
 		--no-interactive)
 			ask_become_pass=""
-			skip_tags="$skip_tags --skip-tags require_google_auth"
+			skip_tags="$skip_tags --skip-tags require_google_auth set_locale"
 			shift 1;;
 	esac
 
@@ -22,5 +27,5 @@ sudo apt update
 sudo apt install --yes ansible
 
 ansible-galaxy install robertdebock.cargo
-ansible-playbook ansible/setup.yaml $ask_become_pass $skip_tags
+ansible-playbook ansible/setup.yaml $ask_become_pass $skip_tags --extra-vars="force_install=$force_install"
 
