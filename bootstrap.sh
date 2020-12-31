3c4cdbd0440cece7919928803d26020906cf9ff2
qwerty <qwerty2501@live.jp>
2020-12-31 17:25:45 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
 
 set -eu
-extract_dir=$(mktemp -u)
-zip_file=$(mktemp -u)
+cd $HOME
 
-trap "
-	rm $zip_file
-	rm -rf $extract_dir
-" 1 2 3 9 15 0
-
-author="qwerty2501"
-repo_name="dotfiles"
-repo_url="https://github.com/$author/$repo_name/archive/master.zip"
-wd="$extract_dir/$repo_name-master"
-if command -v curl > /dev/null 2>&1; then
-	curl -L --output $zip_file $repo_url
-elif command -v wget > /dev/null 2>&1; then
-	wget -O $zip_file $repo_url
-else 
-	echo "invalid environments"
-	exit 1
-fi
-
-unzip -d $extract_dir $zip_file
+sudo apt update
+sudo apt install --yes git
+git clone https://github.com/qwerty2501/dotfiles.git
+cd $HOME/dotfiles
 
 bash $wd/setup.sh $@
 
